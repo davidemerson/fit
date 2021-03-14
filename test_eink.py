1561cb9287eb4735f7639dc4c03e467c949e4d72
David Emerson <d@nnix.com>
2021-03-14 12:58:29 -0400

    Update test_eink.py

--- test_eink.py
+++ test_eink.py
@@ -1,7 +1,7 @@
 import sys
 import os
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib/waveshare_epd')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
